d137a434a8eee690adb1b00d481a2eea6968685d
Karl-John Chow <Karl.chow92@gmail.com>
2016-03-23 10:22:46 +0800

    [Initial commit. Make sure that you med-library-android on the right path. Otherwise configure settings.graddle.]

---
+++ Drone/app/src/main/java/com/drone/drone/drone/activity/BaseActivity.java
@@ -0,0 +1,26 @@
+package com.drone.drone.drone.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+import com.drone.drone.drone.application.ApplicationModel;
+
+/**
+ * Created by karl-john on 18/3/16.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    private ApplicationModel application;
+
+    public ApplicationModel getModel(){
+        if (application == null) {
+            application = (ApplicationModel) getApplication();
+        }
+        return application;
+    }
+
+    public void startActivity(Class <?> cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
+    }
+}
---
+++ Drone/app/src/main/java/com/drone/drone/drone/application/ApplicationModel.java
@@ -0,0 +1,9 @@
+package com.drone.drone.drone.application;
+
+import android.app.Application;
+
+/**
+ * Created by karl-john on 18/3/16.
+ */
+public class ApplicationModel extends Application {
+}
