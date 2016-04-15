13dc85233472d983faea3ae7db45d50fcde782b1
med <gaillysu@med-corp.net>
2016-04-15 19:06:51 +0800

    [fixing] [fix world clock class]

--- Drone/app/src/main/java/com/dayton/drone/ble/model/request/worldclock/GetWorldClockRequest.java
+++ Drone/app/src/main/java/com/dayton/drone/ble/model/request/worldclock/GetWorldClockRequest.java
@@ -1,7 +1,25 @@
 package com.dayton.drone.ble.model.request.worldclock;
 
+import android.content.Context;
+
+import com.dayton.drone.ble.model.request.base.RequestBase;
+
 /**
  * Created by med on 16/4/13.
  */
-public class GetWorldClockRequest {
+public class GetWorldClockRequest extends RequestBase {
+    public final static byte HEADER = (byte)0x07;
+    public GetWorldClockRequest(Context context) {
+        super(context);
+    }
+
+    @Override
+    public byte[][] getRawDataEx() {
+        return new byte[][]{{(byte) 0x80,HEADER}};
+    }
+
+    @Override
+    public byte getHeader() {
+        return HEADER;
+    }
 }
--- Drone/app/src/main/java/com/dayton/drone/ble/model/request/worldclock/SetWorldClockRequest.java
+++ Drone/app/src/main/java/com/dayton/drone/ble/model/request/worldclock/SetWorldClockRequest.java
@@ -1,7 +1,38 @@
 package com.dayton.drone.ble.model.request.worldclock;
 
+import android.content.Context;
+
+import com.dayton.drone.ble.model.request.base.RequestBase;
+
 /**
  * Created by med on 16/4/13.
  */
-public class SetWorldClockRequest {
+public class SetWorldClockRequest extends RequestBase{
+    public final static byte HEADER = (byte)0x06;
+
+    int timeZoneCount;
+    long timeStamp;
+    String timeZoneName;
+
+    public SetWorldClockRequest(Context context,int timeZoneCount, long timeStamp,String timeZoneName) {
+        super(context);
+        this.timeZoneCount = timeZoneCount;
+        this.timeStamp = timeStamp;
+        this.timeZoneName = timeZoneName;
+    }
+
+    @Override
+    public byte[][] getRawDataEx() {
+        return new byte[][]{
+                {
+                        //TODO :fill this packet by lastest spec.
+                        (byte) 0x80,HEADER
+                }
+        };
+    }
+
+    @Override
+    public byte getHeader() {
+        return HEADER;
+    }
 }
