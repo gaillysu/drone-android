7b409117c7cf4ee8e9e24152293366491ddcaeb0
med <gaillysu@med-corp.net>
2017-06-15 16:46:19 +0800

    [fixing] [calibrate hands  continuously  by long press button, and stop it when release long press button]

--- Drone/app/src/main/java/com/dayton/drone/activity/tutorial/CalibrateWatchHourActivity.java
+++ Drone/app/src/main/java/com/dayton/drone/activity/tutorial/CalibrateWatchHourActivity.java
@@ -2,6 +2,9 @@ package com.dayton.drone.activity.tutorial;
 
 import android.os.Bundle;
 import android.os.ResultReceiver;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.dayton.drone.R;
@@ -11,15 +14,23 @@ import com.dayton.drone.ble.model.request.StartSystemSettingRequest;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnLongClick;
 
 /**
  * Created by boy on 2016/4/20.
  */
-public class CalibrateWatchHourActivity extends BaseActivity  {
+public class CalibrateWatchHourActivity extends BaseActivity implements View.OnTouchListener {
 
     @Bind(R.id.clockHour_ImageView)
     ImageView hourImageView;
 
+    @Bind(R.id.calibrate_watch_reverse_imagebutton)
+    ImageButton reverseImageButton;
+
+    @Bind(R.id.calibrate_watch_advance_imagebutton)
+    ImageButton advanceImageButton;
+
+    boolean isLongPress = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,8 @@ public class CalibrateWatchHourActivity extends BaseActivity  {
         setContentView(R.layout.activity_calibrate_watch_hour);
         ButterKnife.bind(this);
         getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.Start.rawValue());
+        reverseImageButton.setOnTouchListener(this);
+        advanceImageButton.setOnTouchListener(this);
     }
 
     @Override
@@ -39,10 +52,22 @@ public class CalibrateWatchHourActivity extends BaseActivity  {
     public void reverseCalibrate(){
         getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.HourHandReverseOneStep.rawValue());
     }
+    @OnLongClick(R.id.calibrate_watch_reverse_imagebutton)
+    public boolean reverseCalibrateContinuously(){
+        isLongPress = true;
+        getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.HourHandReverseMoreSteps.rawValue());
+        return true;
+    }
 
     @OnClick(R.id.calibrate_watch_advance_imagebutton)
     public void advanceCalibrate(){
         getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.HourHandAdvanceOneStep.rawValue());
+    }
+    @OnLongClick(R.id.calibrate_watch_advance_imagebutton)
+    public boolean advanceCalibrateContinuously(){
+        isLongPress = true;
+        getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.HourHandAdvanceMoreSteps.rawValue());
+        return true;
     }
 
     @OnClick(R.id.calibrate_next_page_button)
@@ -55,4 +80,13 @@ public class CalibrateWatchHourActivity extends BaseActivity  {
         },CalibrateWatchMinuteActivity.class);
     }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_UP && isLongPress)
+        {
+            isLongPress = false;
+            getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.StopAllHands.rawValue());
+        }
+        return false;
+    }
 }
--- Drone/app/src/main/java/com/dayton/drone/activity/tutorial/CalibrateWatchSecondActivity.java
+++ Drone/app/src/main/java/com/dayton/drone/activity/tutorial/CalibrateWatchSecondActivity.java
@@ -2,6 +2,9 @@ package com.dayton.drone.activity.tutorial;
 
 import android.os.Bundle;
 import android.os.ResultReceiver;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.dayton.drone.R;
@@ -12,13 +15,22 @@ import com.dayton.drone.ble.model.request.StartSystemSettingRequest;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnLongClick;
 
 /**
  * Created by boy on 2016/4/20.
  */
-public class CalibrateWatchSecondActivity extends BaseActivity  {
+public class CalibrateWatchSecondActivity extends BaseActivity implements View.OnTouchListener {
     @Bind(R.id.clockSecond_ImageView)
     ImageView secondImageView;
+
+    @Bind(R.id.calibrate_watch_reverse_imagebutton)
+    ImageButton reverseImageButton;
+
+    @Bind(R.id.calibrate_watch_advance_imagebutton)
+    ImageButton advanceImageButton;
+
+    boolean isLongPress = false;
 
 
     @Override
@@ -26,6 +38,8 @@ public class CalibrateWatchSecondActivity extends BaseActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calibrate_watch_second);
         ButterKnife.bind(this);
+        reverseImageButton.setOnTouchListener(this);
+        advanceImageButton.setOnTouchListener(this);
     }
 
     @OnClick(R.id.calibrate_watch_reverse_imagebutton)
@@ -33,9 +47,23 @@ public class CalibrateWatchSecondActivity extends BaseActivity  {
         getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.SecondHandReverseOneStep.rawValue());
     }
 
+    @OnLongClick(R.id.calibrate_watch_reverse_imagebutton)
+    public boolean reverseCalibrateContinuously(){
+        isLongPress = true;
+        getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.SecondHandReverseMoreSteps.rawValue());
+        return true;
+    }
+
     @OnClick(R.id.calibrate_watch_advance_imagebutton)
     public void advanceCalibrate(){
         getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.SecondHandAdvanceOneStep.rawValue());
+    }
+
+    @OnLongClick(R.id.calibrate_watch_advance_imagebutton)
+    public boolean advanceCalibrateContinuously(){
+        isLongPress = true;
+        getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.SecondHandAdvanceMoreSteps.rawValue());
+        return true;
     }
 
     @OnClick(R.id.calibrate_next_page_button)
@@ -48,5 +76,15 @@ public class CalibrateWatchSecondActivity extends BaseActivity  {
     public void onBackPressed() {
         super.onBackPressed();
         finishActivityWithResultReceiver((ResultReceiver)getIntent().getExtras().getParcelable(getString(R.string.activity_finisher_key)));
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_UP && isLongPress)
+        {
+            isLongPress = false;
+            getModel().getSyncController().calibrateWatch(StartSystemSettingRequest.AnalogMovementSettingOperationID.StopAllHands.rawValue());
+        }
+        return false;
     }
 }
